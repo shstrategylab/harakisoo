8f29eee684193907f37251247dcf75efeb8bdd8f
shstrategylab <shstrategylab@gmail.com>
2026-06-14 17:49:09 +0900

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // data/ 폴더를 Next.js 번들에서 제외 (API route에서만 fs로 읽음)
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
+  },
 };
 module.exports = nextConfig;
